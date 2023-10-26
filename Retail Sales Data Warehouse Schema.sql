757e81c36993bfb043198180a39ec26fea47086c
D:Steve <35042304+Cusith@users.noreply.github.com>
2023-10-25 22:27:03 -0700

    Update Retail Sales Data Warehouse Schema.sql

--- Retail Sales Data Warehouse Schema.sql
+++ Retail Sales Data Warehouse Schema.sql
@@ -1,52 +1,52 @@
 -- Fact Table
 CREATE TABLE Sales_Fact (
-    sale_id INT PRIMARY KEY,
-    date_id INT,
-    product_id INT,
-    store_id INT,
-    customer_id INT,
-    sales_amount DECIMAL(10, 2),
-    quantity_sold INT,
+    sale_id NUMBER PRIMARY KEY,
+    date_id NUMBER,
+    product_id NUMBER,
+    store_id NUMBER,
+    customer_id NUMBER,
+    sales_amount NUMBER(10, 2),
+    quantity_sold NUMBER,
     -- other relevant metrics
-    FOREIGN KEY (date_id) REFERENCES Date_Dim(date_id),
-    FOREIGN KEY (product_id) REFERENCES Product_Dim(product_id),
-    FOREIGN KEY (store_id) REFERENCES Store_Dim(store_id),
-    FOREIGN KEY (customer_id) REFERENCES Customer_Dim(customer_id)
+    CONSTRAINT fk_date FOREIGN KEY (date_id) REFERENCES Date_Dim(date_id),
+    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES Product_Dim(product_id),
+    CONSTRAINT fk_store FOREIGN KEY (store_id) REFERENCES Store_Dim(store_id),
+    CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES Customer_Dim(customer_id)
 );
 
 -- Dimension Tables
 CREATE TABLE Date_Dim (
-    date_id INT PRIMARY KEY,
+    date_id NUMBER PRIMARY KEY,
     calendar_date DATE,
-    day_of_week VARCHAR(10),
-    month VARCHAR(20),
-    quarter VARCHAR(10),
-    year INT
+    day_of_week VARCHAR2(10),
+    month VARCHAR2(20),
+    quarter VARCHAR2(10),
+    year NUMBER
     -- other time-related attributes
 );
 
 CREATE TABLE Product_Dim (
-    product_id INT PRIMARY KEY,
-    product_name VARCHAR(50),
-    category VARCHAR(30),
-    price DECIMAL(8, 2),
-    inventory_level INT
+    product_id NUMBER PRIMARY KEY,
+    product_name VARCHAR2(50),
+    category VARCHAR2(30),
+    price NUMBER(8, 2),
+    inventory_level NUMBER
     -- other product-related attributes
 );
 
 CREATE TABLE Store_Dim (
-    store_id INT PRIMARY KEY,
-    store_name VARCHAR(50),
-    location VARCHAR(50),
-    region VARCHAR(30)
+    store_id NUMBER PRIMARY KEY,
+    store_name VARCHAR2(50),
+    location VARCHAR2(50),
+    region VARCHAR2(30)
     -- other store-related attributes
 );
 
 CREATE TABLE Customer_Dim (
-    customer_id INT PRIMARY KEY,
-    customer_name VARCHAR(50),
-    email VARCHAR(50),
-    age INT,
-    gender VARCHAR(10)
+    customer_id NUMBER PRIMARY KEY,
+    customer_name VARCHAR2(50),
+    email VARCHAR2(50),
+    age NUMBER,
+    gender VARCHAR2(10)
     -- other customer-related attributes
 );
